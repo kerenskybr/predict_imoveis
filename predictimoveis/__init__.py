6e374e594498131f482eb5155bd731da3684e9a2
Kerenskybr <roger.o.monteiro@gmail.com>
2019-06-04 21:42:06 -0300

    Updated __init__

--- predictimoveis/__init__.py
+++ predictimoveis/__init__.py
@@ -27,8 +27,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
-
-db.create_all()
+#comment before send to heroku
+#db.create_all()
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
